a8892cf767547c72eea3870d17c397a4c66ecdaf
Watife <watife14@yahoo.com>
2018-08-01 23:24:41 +0100

    almost done (delete remaining)

--- app.js
+++ app.js
@@ -1,30 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
-const knex = require('knex')
+const knex = require('knex');
+
 
 
 const app = express();
 app.use(bodyParser.json()); 
 app.use(cors());
 
-// const database = {
-//     users: [
-//         {
-//             id: '123',
-//             name: 'john',
-//             password: 'cookies',
-//             email: 'john@gmail.com',
-//             totalBudget: 0,
-//             totalIncome: 0,
-//             totalExpense: 0,
-//             allBudget: {
-//                 inc: [],
-//                 exp: []
-//             } 
-//         }
-//     ]
-// }
 
 const dbs = knex({
     client: 'pg',
@@ -36,86 +21,158 @@ const dbs = knex({
     }
 });
 
-
-// const db = database.users[0];
-
 let totalInc = 0;
 let totalExp = 0;
 
-app.get('/', (req, res) => {
-    res.json(db.allBudget.inc)
-})
-
 // -------- Register ------------------
 app.post('/register', (req, res) => {
-    const { email, name } = req.body;
-    dbs('users')
-        .returning('*')
-        .insert({
-            email: email,
-            name: name
-        }).then(user => {
-            res.json(user[0]);
+    const { email, name, password } = req.body;
+    const hash = bcrypt.hashSync(password);
+    dbs.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
         })
-        .catch(err => res.status(400).json('email already exists'))
+            .into('login')
+            .returning('email')
+            .then(loginEmail => {
+                return trx('users')
+                    .returning('*')
+                    .insert({
+                        email: loginEmail[0],
+                        name:  name
+                    }).then(user => {
+                        console.log(user[0]);
+                        res.json(user[0]);
+                    })
+            })
+            .then(trx.commit)
+            .catch(trx.rollback)
+    })
 })
+
+// ------SignIn Api --------------
+app.post('/signin', (req, res) => {
+    if (!req.body.email || !req.body.password) {
+        return res.status(400).json('incorrect login details');
+    }
+    dbs.select('email', 'hash').from('login')
+        .where('email', '=', req.body.email)
+        .then(data => {
+            const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+
+            if (isValid) {
+                return dbs.select('*').from('users')
+                    .where('email', '=', req.body.email)
+                    .then(user => {
+                        res.json(user[0])
+                    })
+                    .catch(err => res.status(400).json('unable to get users'))
+            } else {
+                res.status(400).json('wrong credentials')
+            }
+        })
+        .catch(err => res.status(400).json('wrong credentials'))
+})
+
+
+
+//-------------Incomearrays Api------------------
+app.post('/incomearrays', (req, res) => {
+    const {userid} = req.body;
+    dbs('incomeitem').where('userid', '=', userid).select('*')
+    .then(incomearrays => {
+        res.json(incomearrays)
+        }).catch(err => res.status(400).json('cannot retrieve the income arrays'))
+    
+})
+
+
+//-------------Expensearrays Api------------------
+app.post('/expensearrays', (req, res) => {
+    const { userid } = req.body;
+    dbs('expenseitem').where('userid', '=', userid).select('*')
+        .then(expensearrays => {
+            res.json(expensearrays)
+        }).catch(err => res.status(400).json('cannot retrieve the expense arrays'))
+
+})
+
+
+
 
 //---------AddIncItem api---------------
 app.post('/incitem', (req, res) => {
-    const {id, type, value, description} = req.body;
+    const {userid, incid, type, value, description} = req.body;
+
         dbs('incomeitem').returning('*')
-        .insert({
-            id: id,
-            type: type,
-            value: value,
-            description: description
-        }).then(item => {
-            totalInc = totalInc + parseInt(item[0].value);
-            res.json(item[0])
+            .insert({
+                userid: userid,
+                incid: incid,
+                type: type,
+                value: value,
+                description: description
+            }).into('incomeitem').then(item => {
+                res.json(item[0])
             }).catch(err => res.status(400).json('add income error'))
+     
 })
 
 //---------AddExpItem api---------------
 app.post('/expitem', (req, res) => {
-    const { id, type, value, description, percentage } = req.body;
+    const { userid, expid, type, value, description, percentage } = req.body;
     
     dbs('expenseitem').returning('*')
         .insert({
-            id: id,
+            userid: userid,
+            expid: expid,
             type: type,
             value: value,
-            description: description
-        }).then(item => {
+            description: description,
+            percentage: percentage
+        }).into('expenseitem').then(item => {
             res.json(item[0])
-        }).catch(err => res.status(400).json('add expense error'))
+        })
 })
 
 // ---------Income Api--------------
 app.post('/income', (req, res) => {
-    const {id, type, description, value} = req.body;
-    if (type == 'inc' && description !== '' && value !== '' ) {
+    const {userid} = req.body;
+    dbs('incomeitem').where('userid', '=', userid).select('value')
+    .then(response => {
+        const val = response.reduce((acc, value) => {
+            return parseInt(value.value) + acc
+        }, 0)
         dbs('users')
-            .where('id', '=', id).returning('totalincome')
+            .where('userid', '=', userid).returning('totalincome')
             .update({
-                totalincome: totalInc
+                totalincome: val
             }).then(totalincome => {
-                res.json(totalincome[0])
+                console.log(totalincome);
+                res.json(parseInt(totalincome[0]))
             }).catch(err => res.status(400).json('income update error'))
-    }
+        
+    })
 })
 
 // ---------Expense Api------------------
 app.post('/expense', (req, res) => {
-    const { type, description, value } = req.body;
-    if (type == 'exp' && description !== '' && value !== '') {
-        dbs('users')
-            .where('id', '=', id).returning('totalexpense')
-            .update({
-                totalexpense: totalExp
-            }).then(totalexpense => {
-                res.json(totalexpense[0])
-            }).catch(err => res.status(400).json('expense update error'))
-    }
+    const { userid } = req.body;
+    dbs('expenseitem').where('userid', '=', userid).select('value')
+        .then(response => {
+            const val = response.reduce((acc, value) => {
+                return parseInt(value.value) + acc
+            }, 0)
+            dbs('users')
+                .where('userid', '=', userid).returning('totalexpense')
+                .update({
+                    totalexpense: val
+                }).then(totalexpense => {
+                    console.log(totalexpense);
+                    res.json(parseInt(totalexpense[0]))
+                }).catch(err => res.status(400).json('expense update error'))
+
+        })
 })
 
 // ---------DeleteInc Api----------------
@@ -141,3 +198,10 @@ app.post('/expdelete', (req, res) => {
 app.listen(8080, () => {
     console.log('app is listening');
 })
+
+
+
+// register ----
+
+//i want to get the emmail, and password, nd name and send back, the 
+// details
